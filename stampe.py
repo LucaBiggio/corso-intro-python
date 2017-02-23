04ad577c2bb11d3b384826daa0d6d44e59c2bb20
LucaBiggio <lucabiggio519@gmail.com>
2017-02-23 16:04:24 +0100

    un sacco di stampe

--- stampe.py
+++ stampe.py
@@ -14,4 +14,41 @@ print 3 > 4
 
 print 3 < 10
 
+name = "Luma"
+age = 18
+sentence = "Ciao Luma guarda"
+sentence1 = "come mi diverto"
+
+print "Ciao, mi chiamo %s e ho %s anni" % (name,age)
+print "Ciao sono %s" % name
+ 
+delta = 10
+template = "Ciao mi chiamo %s e ho %s anni"
+
+print "Ciao mi chiamo %s e 10 anni fa avevo %s anni" % (name, age-10)
+
+print "Ciao mi chiamo %s e %s anni fa avevo %s anni" % (name, delta, age-delta)
+print template % (name,age)
+print sentence + " " + sentence1 
+print """
+I topi
+non avevano nipoti
+"""
+
+print "Ciao. \n mi chiamo \n\n e ho 18 anni."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
